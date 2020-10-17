d8f0c9f7867cc245a435981fc26fb3e14caf8b2b
Juanra <juanrgmere@gmail.com>
2020-10-17 19:21:05 +0200

    Añadido adc_test.py como primera iniciación a controlar el ADC ADS1015 (todavía no acabado)

--- drafts/adc_test.py
+++ drafts/adc_test.py
@@ -4,7 +4,11 @@ import smbus
 
 DEVICE_BUS = 1
 DEVICE_ADDRESS = 0x48
+CONFIG_REGISTER = 0X01
 ALERT_READY_PIN = 40
 
-alert_ready = DigitalInputDevice(4ALERT_READY_PIN, pull_up = True)
+alert_ready = DigitalInputDevice(ALERT_READY_PIN, pull_up = True)
 bus = smbus.SMBus(1)
+
+val = bus.read_i2c_block_data(DEVICE_ADDRESS, CONFIG_REGISTER, 2)
+print("Old CONFIG:", val)
