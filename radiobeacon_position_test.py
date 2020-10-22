c9ab272fea11efd14f0ecf2e0697f8adcbd4ee80
UO250487 <uo250487@uniovi.es>
2020-10-22 16:44:42 +0200

    Modificado script de test

--- radiobeacon_position_test.py
+++ radiobeacon_position_test.py
@@ -1,6 +1,8 @@
 from systems.ads1015 import ADS1015
 from gpiozero import DigitalInputDevice
 import smbus
+import time
+import statistics
 
 I2C_BUS = 1
 DEVICE_I2C_ADDRESS = 0x48
@@ -10,8 +12,16 @@ ready_signal = DigitalInputDevice(ALERT_READY_SIGNAL, pull_up=True)
 i2c_bus = smbus.SMBus(1)
 adc = ADS1015(i2c_bus, DEVICE_I2C_ADDRESS, ready_signal)
 
-f" Old config: {adc.read_config()} \n"
+fifo = [0] * 10
+print(f" Old config: {adc.read_config()} \n")
 
+counter = 0
 while True:
     phase_diff_voltage = adc.read_single_shot_debug(0, 6.144, 490)
-    f" {phase_diff_voltage} V \n"
+    fifo[1:10] = fifo[0:9]
+    fifo[0] = phase_diff_voltage
+    avg = statistics.mean(fifo)
+    counter += 1
+    if counter % 10 == 0:
+        print(f"Actual: {phase_diff_voltage} V \t Media: {avg} V")
+    time.sleep(0.05)
