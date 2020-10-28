eb6b74a06d9d027171ce7dfc29b9eb0cc10c1985
Efren <efrenestradaescobio@gmail.com>
2020-10-28 12:57:58 +0100

    Añadido script de prueba del Sense Hat

--- drafts/gps_test2.py
+++ drafts/gps_test2.py
@@ -6,25 +6,19 @@ import time
 
 print("Starting - PROJECT VERNE")
 
-serial_io = serial.Serial("/dev/serial0", 9600, timeout=2.0)
-# serial_io = io.TextIOWrapper(io.BufferedRWPair(serial_port, serial_port))
-# serial_io = io.TextIOWrapper(serial_port)
+serial_port = serial.Serial("/dev/serial0", 9600, timeout=5.0)
+serial_io = io.TextIOWrapper(io.BufferedRWPair(serial_port, serial_port))
 
-timestamp = time.time()
 while 1:
     try:
-        time.sleep(3)
-        serial_io.flushInput()
-        line = serial_io.readline().decode("UTF-8")
+        line = serial_io.readline()
         msg = pynmea2.parse(line)
-        if msg.sentence_type == "GGA":
+        #print(msg.sentence_type)
+        print(msg.sentence_type)
+        print(repr(pynmea2.GGA))
+        if msg.sentence_type == pynmea2.GGA:
             print(repr(msg))
-        # new_time = time.time()
-        # if (new_time-timestamp) > 0.1:
-        #     print(f"Time since last: {new_time - timestamp}")
-        # timestamp = new_time
-
-
+            time.sleep(0.5)
     except serial.SerialException as e:
         print('Device error: {}'.format(e))
         break
